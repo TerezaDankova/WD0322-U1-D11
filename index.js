83e90ca98a5f370c09d01261dd0feabe4cf2aba4
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-07-12 15:14:53 +0200

    update index.js

--- index.js
+++ index.js
@@ -15,25 +15,9 @@ const toggleAllHeaderClasses = function () {
     }
  
 function changeBackgroundColor() {
-  document.body.style.backgroundColor = "#65adb8";
+  document.body.style.backgroundColor = "#a6d1f6";
 }
 
-/*
-//n.12 exercise
- function amazonlink(changecolor) {
-   let amazonLinksCssNode = document.getElementsByClassName("amazonlink")
-   for(let i=0 ; i < amazonLinksCssNode.length ; i++)
- } {
- amazonLinksCssNode[i].style.color = changecolor 
- }
- //n.12 exercise
- function colorOfPrice (){
-   let changePriceColorNode = document.getElementsByClassName("cost")
-   for(let i = 0; i < changePriceColorNode.length; i++){
-       const red = Math.round(Math.random() * 255)
-       const green = Math.round(Math.random() * 255)
-       const blue = Math.round(Math.random() * 255)
-       changePriceColorNode[i].style.color = "rgb(" + red + ", " + green + "," + blue + ")";
-     }
- }
-*/
+function footeradress() {
+  document.getElementById("footer").innerText = "buyithere@gmail.com"
+}
